97c1106290bfd594f3044bcb7e00f9fb5094dae3
VolkovskayaUliana <uliana.volkovskaya@smekalka.com>
2023-08-15 00:33:49 +0700

    feat: change auth on next-auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
 import styles from "./rootPage.module.css";
 import StandingGang from "@/components/funnyJokes/StandingGang";
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/app/api/auth/[...nextauth]/route";
 
-const Main = () => {
-  return (
-    <div className={styles.page}>
-      <div className={styles.pageElementRow}>
-        <h1>Главная</h1>
-      </div>
-      <StandingGang />
-    </div>
-  );
+const Main = async () => {
+    const session = await getServerSession(authOptions)
+
+    console.log(session,"session")
+    return (
+        <div className={styles.page}>
+            <div className={styles.pageElementRow}>
+                <h1>Главная</h1>
+            </div>
+            <StandingGang/>
+        </div>
+    );
 };
 
 export default Main;
